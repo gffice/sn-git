d975b9ef70fd87e3c4ec692d70da1e3efb2fdfa8
Cecylia Bocovich <cohosh@torproject.org>
2026-01-13 09:31:24 -0500

    Add The Tor Project, Inc to license in backoff.go

--- common/task/backoff.go
+++ common/task/backoff.go
@@ -4,6 +4,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2015-2021 V2Ray & V2Fly Community
+Copyright (c) 2026 The Tor Project, Inc
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
